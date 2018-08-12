8c74390eb1a7bc8e1119438a3b9b695c30b7fefc
Bruno casotto <casottoalves@hotmail.com>
2018-08-11 23:11:30 -0300

    Button buy done

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -6,12 +6,11 @@ class Button extends React.Component {
 
   render() {
     return (
-      <button className="btn">
-        teste
+      <button onClick={this.props.onAction} className="btn btn--primary" >
+        {this.props.text}
       </button>
     )
   }
-
 }
 
 export default Button
--- src/components/info-box/index.jsx
+++ src/components/info-box/index.jsx
@@ -1,7 +1,13 @@
 
 import React from 'react'
 import './_styles.css'
+import Button from '_components/button'
+
 class InfoBox extends React.Component {
+
+  buy() {
+    alert('Comprar item')
+  }
 
   render() {
     return (
@@ -9,6 +15,12 @@ class InfoBox extends React.Component {
         <div className="info-box__info">
           <h1 className="title">{this.props.title}</h1>
           <p className="sub-title">{this.props.price}</p>
+        </div>
+        <div className="info-box__controller">
+          <Button 
+            text="Comprar"
+            onAction={this.buy}
+          />
         </div>
       </div>
     )
