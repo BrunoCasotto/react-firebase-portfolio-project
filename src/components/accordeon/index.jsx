c029ceae6865565084ca33866e2a06aefb4b088c
Bruno <casottoalves@hotmail.com>
2018-08-13 17:07:17 -0300

    update accordeon component structure

--- src/components/accordeon/index.jsx
+++ src/components/accordeon/index.jsx
@@ -3,16 +3,12 @@ import React from 'react'
 import './_styles.css'
 
 class Accordeon extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = { appearance: 'closed' }
-  }
+  state = { appearance: 'closed' }
 
   /**
    * function to open and close accordeon
    */
-  toggle() {
+  toggle = () => {
     let newAppearance = this.state.appearance === 'closed' ? 'open' : 'closed'
 
     this.setState({
@@ -26,7 +22,7 @@ class Accordeon extends React.Component {
 
   render() {
     return (
-      <div onClick={this.toggle.bind(this)} className={`accordeon fas accordeon--${this.state.appearance}`}>
+      <div onClick={this.toggle} className={`accordeon fas accordeon--${this.state.appearance}`}>
         <div className="accordeon__content"
           dangerouslySetInnerHTML={this.createMarkup()}
         />
