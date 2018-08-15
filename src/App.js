d91e5cf944568f988f8607c393a96852a631e8a0
Bruno <casottoalves@hotmail.com>
2018-08-15 13:44:24 -0300

    remove components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
 import '_styles/global.css'
 import '_styles/reset.css'
-import './App.css'
-import ProductPage from '_pages/Product.jsx'
 import Header from '_components/header/index.jsx'
 
 class App extends Component {
@@ -10,7 +8,6 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <ProductPage />
       </div>
     )
   }
