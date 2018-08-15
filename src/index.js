154f7650611561f502378423569c8ffa2395eedb
Bruno <casottoalves@hotmail.com>
2018-08-15 14:11:48 -0300

    remove project

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '_styles/global.scss'
-import '_styles/reset.scss'
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
