79596f2bb9d8060d3442d4d6570ccf1a77a3aca1
Bruno <casottoalves@hotmail.com>
2018-08-16 15:08:56 -0300

    Battery separated by component

--- src/components/personal-skills/index.jsx
+++ src/components/personal-skills/index.jsx
@@ -1,29 +1,17 @@
 import React from 'react'
 import './styles.css'
+import Battery from '../battery'
 
 const PersonalSkills = ({skills}) => {
-
-  const renderBattery = (percent) => {
-    return <div class='battery'>
-      <div class='bar active' data-power='10'></div>
-      <div class='bar' data-power='20'></div>
-      <div class='bar' data-power='30'></div>
-      <div class='bar' data-power='40'></div>
-      <div class='bar' data-power='50'></div>
-      <div class='bar' data-power='60'></div>
-      <div class='bar' data-power='70'></div>
-      <div class='bar' data-power='80'></div>
-      <div class='bar' data-power='90'></div>
-      <div class='bar' data-power='100'></div>
-    </div>
-  }
 
   const renderSkillList = (list) => {
     return list.map(skill => {
       return <div className="personal-skills__graphic">
         <h3 className="title title--small title--slim personal-skills__graphic__title">{skill.title}</h3>
         <div className="personal-skills__battery">
-          {renderBattery(skill.percent)}
+        <Battery
+          percent={skill.percent}
+        />
         </div>
       </div>
     })
