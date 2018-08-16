bce557244d7a3e8f0cf9d05acd3126d2c4a1ec8d
Bruno <casottoalves@hotmail.com>
2018-08-16 13:12:45 -0300

    Title and sub title done

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import Avatar from './components/avatar'
 import './app.css'
 
 class App extends Component {
+  state = {
+    socialList: [
+      {icon: 'github', 'url': 'https://github.com/brunocasotto'},
+      {icon: 'facebook', 'url': 'https://www.facebook.com/profile.php?id=100001318441034'},
+      {icon: 'linkedin', 'url': 'https://www.linkedin.com/in/brunocasotto/'}
+    ]
+  }
+
   render() {
     return (
       <div className="main">
-        <Header />
+        <Header 
+          socialList={this.state.socialList}
+        />
         <div className="main__content">
           <div className="main__content__avatar">
             <Avatar />
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import './styles.css'
 
-const Header = () => {
+const Header = ({socialList}) => {
+
+  const renderSocial = (socialList) => {
+    return socialList.map( social => {
+      return <a href={social.url} target="_blank">
+        <i class={`fab fa-${social.icon} icon`}></i>
+      </a>
+    })
+  }
 
   return (
     <header className="header">
       <h1 className="title title--big header__title">Bruno Casotto</h1>
+      <h2 className="title title--slim header__subtitle">Front end enginner</h2>
+
+      <div className="header__social">
+        {renderSocial(socialList)}
+      </div>
     </header>
   )
 }
