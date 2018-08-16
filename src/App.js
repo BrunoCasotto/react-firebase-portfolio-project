b00b1a80d24d96fe948df0150abf4d1582c44b43
Bruno <casottoalves@hotmail.com>
2018-08-16 12:46:56 -0300

    avatar place done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import Header from './components/header'
 import Avatar from './components/avatar'
+import './app.css'
 
 class App extends Component {
   render() {
     return (
       <div className="main">
         <Header />
-        <div className="main-content">
-          <Avatar />
+        <div className="main__content">
+          <div className="main__content__avatar">
+            <Avatar />
+          </div>
         </div>
       </div>
     )
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,7 +5,7 @@ const Header = () => {
 
   return (
     <header className="header">
-      <h1 className="header__title">Bruno Casotto</h1>
+      <h1 className="title title--big header__title">Bruno Casotto</h1>
     </header>
   )
 }
