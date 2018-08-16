497d4ba521794afa30f59f651ebd1bf860a29cde
Bruno <casottoalves@hotmail.com>
2018-08-16 15:20:07 -0300

    percent sum done

--- src/components/battery/index.jsx
+++ src/components/battery/index.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import './styles.css'
 
-const Battery = (percent) => {
+const renderBatteryPercent = (percent) => {
+  const percentList = []
+
+  for(let x = 0; x < 10; x++) {
+    const status = percent >= x * 10 ? 'bar active' : 'bar'
+
+    percentList.push(
+      <div class={status} data-power='10'></div>
+    )
+  }
+
+  return percentList
+}
+const Battery = ({percent}) => {
   return <div class='battery'>
-    <div class='bar active' data-power='10'></div>
-    <div class='bar' data-power='20'></div>
-    <div class='bar' data-power='30'></div>
-    <div class='bar' data-power='40'></div>
-    <div class='bar' data-power='50'></div>
-    <div class='bar' data-power='60'></div>
-    <div class='bar' data-power='70'></div>
-    <div class='bar' data-power='80'></div>
-    <div class='bar' data-power='90'></div>
-    <div class='bar' data-power='100'></div>
+    {renderBatteryPercent(percent)}
   </div>
 }
 
